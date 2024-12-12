09aa0c41a7b179b24ff1f4e515d0ba488f8d698d
Filip Ziolo <fifiziolo@gmail.com>
2024-12-12 17:38:48 +0100

    bcrypt added

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { Pool } = require('pg');
 require('dotenv').config();
+const bcrypt = require('bcryptjs');
 
 
 // Tworzenie aplikacji Express
@@ -28,25 +29,36 @@ app.post('/login', async (req, res) => {
 
     console.log('Logging in user:', { email });
 
-    const result = await pool.query(
-      'SELECT * FROM users WHERE email = $1 AND password = $2',
-      [email, password]
-    );
+  
+    const result = await pool.query('SELECT * FROM users WHERE email = $1', [
+      email,
+    ]);
 
     if (result.rows.length === 0) {
-      res.status(401).json({
+      return res.status(401).json({
         success: false,
         message: 'Invalid email or password.',
       });
-    } else {
-      const user = result.rows[0];
-      console.log('User logged in:', user);
-      res.status(200).json({
-        success: true,
-        message: 'Login successful',
-        name: user.name,
+    }
+
+    const user = result.rows[0];
+
+  
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      return res.status(401).json({
+        success: false,
+        message: 'Invalid email or password.',
       });
     }
+
+    console.log('User logged in:', user);
+    res.status(200).json({
+      success: true,
+      message: 'Login successful',
+      name: user.name,
+    });
   } catch (err) {
     console.error('Login error:', err.message);
     res.status(500).json({
@@ -56,20 +68,30 @@ app.post('/login', async (req, res) => {
   }
 });
 
-// app.get('/signup', async ( req, res) => {
-//   console.log("chuj");
-//  });
+
 app.post('/signup', async (req, res) => {
   try {
     const { name, email, password } = req.body;
 
     console.log('Registering user:', { name, email });
 
+    const existingUser = await pool.query(
+      'SELECT * FROM users WHERE email = $1',
+      [email]
+    );
+
+    if (existingUser.rows.length > 0) {
+      return res.status(400).json({
+        success: false,
+        message: 'Email is already registered.',
+      });
+    }
     
-  
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const result = await pool.query(
       'INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *',
-      [name, email, password]
+      [name, email, hashedPassword]
     );
 
     console.log('User inserted:', result.rows[0]);
